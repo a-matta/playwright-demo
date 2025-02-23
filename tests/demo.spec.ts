6b77a7d12c344a77fd2cc4c59e3c62dda7d59e58
Amrita Matta <matta.amrita@gmail.com>
2025-02-23 20:50:46 +0200

    demo2
    Signed-off-by: Amrita Matta <matta.amrita@gmail.com>

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Login Demo", async ({ page }) => {
+test("Login Demo 1", async ({ page }) => {
   await page.goto("https://demo.applitools.com/");
   const username = page.locator("//input[@id='username']");
   await username.click();
@@ -9,5 +9,19 @@ test("Login Demo", async ({ page }) => {
   await password.click();
   await password.fill("amrita");
   const loginButton = page.locator("//a[@id='log-in']");
+  await loginButton.click();
+});
+
+test("Login Demo 2", async ({ page }) => {
+  await page.goto(
+    "https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F"
+  );
+  const username = page.locator("//input[@id='Email']");
+  await username.click();
+  await username.fill("admin@yourstore.com");
+  const password = page.locator("//input[@id='Password']");
+  await password.click();
+  await password.fill("admin");
+  const loginButton = page.locator("//button[text()='Log in']");
   await loginButton.click();
 });
