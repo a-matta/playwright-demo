61014ef7f9015bd034760f69d923fe66f2cfeb30
Amrita Matta <matta.amrita@gmail.com>
2025-02-25 22:21:44 +0200

    added tags
    Signed-off-by: Amrita Matta <matta.amrita@gmail.com>

--- tests/hooks.spec.ts
+++ tests/hooks.spec.ts
@@ -5,7 +5,8 @@ test.beforeEach(async ({ page }) => {
   await expect(page).toHaveTitle(/Swag Labs/);
 });
 
-test("Login", async ({ page }) => {
+test.only("Login", async ({ page }) => {
+  //Added tag runs only this test
   const username = page.locator("id=user-name");
   await username.click();
   await username.fill("standard_user");
@@ -18,9 +19,18 @@ test("Login", async ({ page }) => {
   await openMenu.click();
 });
 
-test("Logout", async ({ page }) => {
+test.skip("Logout", async ({ page }) => {
+  //added tag skip
   const openMenu = page.locator("id=react-burger-menu-btn");
   await openMenu.click();
+});
+
+test.fixme("Inventory", async ({ page }) => {
+  //added tag fixme
+  await page.goto("https://www.saucedemo.com/inventory.html");
+  await expect(page).toHaveTitle(/Swag Labs/);
+  const header = page.locator("text()=Swag Labs");
+  await header.isVisible();
 });
 
 test.afterEach(async ({ page }) => {
